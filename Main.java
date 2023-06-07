d58e27aa4ac712b00fec38bf2d7f398828dd65ff
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-06-07 18:43:41 +0900

    added addText()

--- Main.java
+++ Main.java
@@ -3,6 +3,11 @@ package githubTest;
 public class Main{
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        addText();
+    }
+
+    public static void addText(){
+        System.out.println("This is add function");
     }
 }
 
